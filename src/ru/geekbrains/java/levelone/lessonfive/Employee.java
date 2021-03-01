824e30ef87e5ef647f0f63b3013746f46aab1d90
Evgeny <mr_orange@inbox.ru>
2021-03-01 09:40:43 +0300

    Lesson Five Begin

---
+++ src/ru/geekbrains/java/levelone/lessonfive/Employee.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java.levelone.lessonfive;
+
+public class Employee {
+    private String name;
+    private String position;
+    private int age;
+    private int phone;
+    private double salary;
+
+
+}
---
+++ src/ru/geekbrains/java/levelone/lessonfive/LessonFiveApp.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.java.levelone.lessonfive;
+
+public class LessonFiveApp {
+}
